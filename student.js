e8789fe68d8f0dcfb759d263fc625d44450000ea
Valeria Spektor <vlapin84@gmail.com>
2024-03-02 16:20:27 +0200

    added changes to student.js

--- student.js
+++ student.js
@@ -1,24 +1,20 @@
-// students.js
-
 function congratulateTopStudent(data) {
-  let maxScore = 0;
   let topStudent = null;
-
-  
 
   data.forEach(group => {
     group.forEach(student => {
-      if (student.score > maxScore) {
-        maxScore = student.score;
-        topStudent = student;
+      if (typeof student.name === 'string' && typeof student.score === 'number') {
+        if (!topStudent || student.score > topStudent.score || (student.score === topStudent.score && new Date(student.date) > new Date(topStudent.date))) {
+          topStudent = student;
+        }
+      } else {
+        // Handle invalid data as needed
       }
     });
   });
-  
+
   if (topStudent) {
-    console.log(`Congratulations to ${topStudent.name} for achieving the highest score: ${maxScore}!`);
-  } else {
-    console.log('No student with a score greater than 0 found.');
+    console.log('Congratulations to ${topStudent.name} for achieving the highest score (${topStudent.score}) and being the fastest with date: ${topStudent.date}!');
   }
 }
 
